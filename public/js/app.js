ca88d1fd95d36f3846d40e9b2d69237c0c493607
Muhammad Taimour <muhammad.taimour95@gmail.com>
2019-07-22 21:31:57 +0500

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log("client side javascript")
+
+
+const weatherform = document.querySelector('form')    
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message1')
+//messageOne.textContent = ''
+const messageTwo = document.querySelector('#message2')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+           
+            if (data.error) {
+                messageOne.textContent = error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecastData
+            }
+        })
+    })
+
+})
---
+++ src/app.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forcast')
+
+//const 
+const app = express()
+const publicDirectoryPath =path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup Static directory
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title:'Weather',
+        name: 'Muhammad Taimour'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title:'About Me',
+        name: 'Muhammad Taimour'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Andrew Mead'
+    })
+})
+
+
+
+app.get('/weather', (req, res) => {
+if(!req.query.address){
+    return res.send({
+        error:'You must provide a valid address'
+    })
+}
+
+geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+        return res.send({
+            error:error
+        })
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return res.send({
+                error:error
+            })
+        }
+
+        res.send({
+            forecastData: forecastData,
+            location: location,
+            address: req.query.address
+        })
+
+       
+    })
+})
+
+
+
+})
+
+
+
+app.listen(3000, () => {
+    console.log("Server is up on port 3000")
+})
